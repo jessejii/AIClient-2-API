b7212d385758607cbd05075281d92f0d6595ea63
jessejii <12292101+jessejii@users.noreply.github.com>
2025-08-14 12:28:02 +0800

    github action

--- healthcheck.js
+++ healthcheck.js
@@ -7,7 +7,7 @@ const http = require('http');
 
 // 从环境变量获取主机和端口，如果没有设置则使用默认值
 const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 7066;
 
 // 发送HTTP请求到健康检查端点
 const options = {
